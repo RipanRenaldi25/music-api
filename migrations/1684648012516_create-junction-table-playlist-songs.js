62b910061fc6661787a2d245ea720c1d20cadba9
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2023-05-21 12:52:14 +0700

    feat(junction table): create junction table on songs and playlist

--- migrations/1684648012516_create-junction-table-playlist-songs.js
+++ migrations/1684648012516_create-junction-table-playlist-songs.js
@@ -2,6 +2,26 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = (pgm) => {
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(255)',
+      primaryKey: true,
+    },
+    id_song: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    id_playlist: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+  });
+  pgm.addConstraint('playlist_songs', 'fk_id_song_from_songs', 'FOREIGN KEY(id_song) REFERENCES songs(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlist_songs', 'fk_id_playlist_from_playlists', 'FOREIGN KEY(id_playlist) REFERENCES playlists(id) ON DELETE CASCADE');
+  pgm.addConstraint('playlist_songs', 'unique_id_song_id_playlist', 'UNIQUE(id_song, id_playlist)');
+};
 
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_songs');
+};
